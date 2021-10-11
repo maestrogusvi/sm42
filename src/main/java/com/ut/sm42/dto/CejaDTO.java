71d9ae3afdd4d5e0df000c95fe62942040eadc9d
CesarCejaC <tonycejacortes@gmail.com>
2021-10-11 08:17:05 -0500

    feat(CejaDTO) Add new DTO

---
+++ src/main/java/com/ut/sm42/dto/CejaDTO.java
@@ -0,0 +1,25 @@
+package com.ut.sm42.dto;
+
+public class CejaDTO {
+    int id;
+    String name;
+
+    public CejaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
