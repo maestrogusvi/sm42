53f3044ea7c629e9661cb6229375079a458ffa28
armando vivanco <armando.vivanco@sapper.ai>
2021-10-11 07:25:49 -0500

    Feat(ExampleDTO ) add new dto

---
+++ src/main/java/com/ut/sm42/dto/ExampleDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm42.dto;
+
+public class ExampleDTO {
+
+    int id;
+    String name;
+
+    public ExampleDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
