36e5e4494ce4d983426c94a4a5de44404230af2a
Diegohuchim17 <perezaledi@gmail.com>
2021-10-11 17:11:29 -0500

    FEAT(HuchimDTO)

---
+++ src/main/java/com/ut/sm42/dto/HuchimDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm42.dto;
+
+public class HuchimDTO {
+
+    int id;
+    String name;
+
+    public HuchimDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
