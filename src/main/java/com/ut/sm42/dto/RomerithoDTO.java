ec6a5deceeb0b7a0811452468d753cd1896dc8ed
sebem <20393097@utcancun.edu.mx>
2021-10-11 07:57:21 -0500

    FEAT(RomerithoDTO)

---
+++ src/main/java/com/ut/sm42/dto/RomerithoDTO.java
@@ -0,0 +1,29 @@
+package com.ut.sm42.dto;
+
+public class RomerithoDTO {
+    int id ;
+    String name;
+
+    public RomerithoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+
+
+}
