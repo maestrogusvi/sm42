2c0776012016e7e965bf5dcd3575ab599fb390b9
noermorales98 <noe.rmorales98@gmail.com>
2021-10-11 08:39:18 -0500

    add(NoeliDTO)

---
+++ src/main/java/com/ut/sm42/dto/NoeliDTO.java
@@ -0,0 +1,25 @@
+package com.ut.sm42.dto;
+
+public class NoeliDTO {
+    int idnoe;
+    String namenoe;
+
+    public NoeliDTO() {
+    }
+
+    public int getIdnoe() {
+        return idnoe;
+    }
+
+    public void setIdnoe(int idnoe) {
+        this.idnoe = idnoe;
+    }
+
+    public String getNamenoe() {
+        return namenoe;
+    }
+
+    public void setNamenoe(String namenoe) {
+        this.namenoe = namenoe;
+    }
+}
