1158beece3ea2ed982264c0936c8a2fb1dc6a681
V0id23 <aldomezamoreno@gmail.com>
2021-10-11 08:11:36 -0500

    feat(MezaDTO)

---
+++ src/main/java/com/ut/sm42/dto/MezaDTO.java
@@ -0,0 +1,29 @@
+package com.ut.sm42.dto;
+
+public class MezaDTO {
+    int id;
+    String name;
+
+    public MezaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
