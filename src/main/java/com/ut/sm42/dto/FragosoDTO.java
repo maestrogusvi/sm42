4559a3d1874e443e03f5d7069de84594ba94042a
lorddeivis <fragosomadera99@gmail.com>
2021-10-11 19:57:12 -0500

    feat(FragosoDTO) add new DTO

---
+++ src/main/java/com/ut/sm42/dto/FragosoDTO.java
@@ -0,0 +1,25 @@
+package com.ut.sm42.dto;
+
+public class FragosoDTO {
+    int id;
+    String name;
+
+    public FragosoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
