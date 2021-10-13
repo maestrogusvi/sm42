e8f6ed3d7e508815b48bc7c1e0ca317ec9bf67d1
carloslair24 <laircarlos24@gmail.com>
2021-10-13 00:04:18 -0500

    FEAT(LairDTO)

---
+++ src/main/java/com/ut/sm42/dto/LairDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm42.dto;
+
+public class LairDTO {
+    int id;
+    String name;
+
+    public LairDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
