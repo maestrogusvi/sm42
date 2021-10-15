27635077b684c009c639429f32f4312b7a11901f
gerryr8 <camaragerardo2000@gmail.com>
2021-10-14 21:28:07 -0500

    Feat(Camara_DTO) new DTO

--- src/main/java/com/ut/sm42/dto/Camara_DTO.java
+++ src/main/java/com/ut/sm42/dto/Camara_DTO.java
@@ -2,21 +2,24 @@ package com.ut.sm42.dto;
 
 public class Camara_DTO {
 
-
     int id;
     String name;
+    String status;
 
-    public Camara_DTO(int id) {
-        this.id = id;
+
+    public Camara_DTO() {
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id;}
 
     public String getName() {
         return name;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
 
     public void setId(int id) {
         this.id = id;
@@ -26,8 +29,10 @@ public class Camara_DTO {
         this.name = name;
     }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
-
 
 
 
