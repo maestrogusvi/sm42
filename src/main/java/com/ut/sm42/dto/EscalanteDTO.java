9b1fb72ab2d11f0f61028c4572386ad320e2e258
Danielescalant28 <serchi2819@gmail.com>
2021-10-17 14:00:19 -0500

    temporal

---
+++ src/main/java/com/ut/sm42/dto/EscalanteDTO.java
@@ -0,0 +1,35 @@
+package com.ut.sm42.dto;
+
+public class EscalanteDTO {
+
+    int id;
+    String status;
+    String name;
+
+    public EscalanteDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/ut/sm42/service/ApplicationService.java
+++ src/main/java/com/ut/sm42/service/ApplicationService.java
@@ -3,6 +3,7 @@ package com.ut.sm42.service;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ut.sm42.dto.BeeceptorDTO;
+import com.ut.sm42.dto.EscalanteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,15 @@ public class ApplicationService {
         beeceptorDTO.setMessage(json.get("message").getAsString());
         beeceptorDTO.setStatus(json.get("status").getAsString());
         return beeceptorDTO;
+    }
+
+    public EscalanteDTO pan() throws IOException {
+        JsonParser parser = new JsonParser();
+        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm41.free.beeceptor.com","GET",null,null,"json",null, null));
+        EscalanteDTO pan = new EscalanteDTO();
+        pan.setId(json.get("id").getAsInt());
+        pan.setName(json.get("name").getAsString());
+        pan.setStatus(json.get("status").getAsString());
+        return pan;
     }
 }
