8d4ac685006d5237d97659509917c7c3b61f4a53
DiegoAsanchezmartin <92271073+DiegoAsanchezmartin@users.noreply.github.com>
2021-10-18 02:04:16 -0500

    Add files via upload

---
+++ src/main/java/com/ut/sm42/dto/DiegoDTO.java
@@ -0,0 +1,35 @@
+package com.ut.sm42.dto;
+public class DiegoDTO {
+    int id;
+    String name;
+    String status;
+
+
+    public DiegoDTO() {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
