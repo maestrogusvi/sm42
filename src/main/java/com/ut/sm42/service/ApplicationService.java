a35b90342f0b5d874fa452ab3c2143fbd245c3bf
noermorales98 <noe.rmorales98@gmail.com>
2021-10-18 14:54:32 -0600

    add(ApplicationServiceImpl) fix(ApplicationService)

--- src/main/java/com/ut/sm42/service/ApplicationService.java
+++ src/main/java/com/ut/sm42/service/ApplicationService.java
@@ -3,7 +3,6 @@ package com.ut.sm42.service;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ut.sm42.dto.*;
-import com.ut.sm42.dto.BeeceptorDTO;
 import com.ut.sm42.exception.BusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,66 +11,13 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
-public class ApplicationService {
+public interface ApplicationService {
+    String firstService();
+    BeeceptorDTO testHttp() throws IOException;
+    NoeliDTO noe() throws  IOException;
+    MezaDTO aldo() throws IOException;
+    FragosoDTO david() throws IOException;
+    CejaDTO cesar()throws IOException;
 
-    @Autowired
-    HttpService httpService;
 
-    public String firstService(){
-        return "service";
-    }
-
-    public BeeceptorDTO testHttp() throws IOException {
-        JsonParser parser = new JsonParser();
-        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm42.free.beeceptor.com","GET",null,null,"json",null, null));
-        BeeceptorDTO beeceptorDTO = new BeeceptorDTO();
-
-        if(json.get("code")== null){
-            throw new BusinessException("Code doesn´t exist", HttpStatus.FORBIDDEN);
-        }
-        beeceptorDTO.setCode(json.get("code").getAsString());
-        beeceptorDTO.setMessage(json.get("message").getAsString());
-        beeceptorDTO.setStatus(json.get("status").getAsString());
-        return beeceptorDTO;
-    }
-
-    //segundo
-    public NoeliDTO noe() throws  IOException{
-        JsonParser par = new JsonParser();
-        JsonObject json = (JsonObject) par.parse(httpService.sendRequestHttpS("https://equiponoe.free.beeceptor.com","GET",null,null,"json",null, null));
-        NoeliDTO noeDTO = new NoeliDTO();
-        noeDTO.setStatus(json.get("status").getAsString());
-        noeDTO.setID(json.get("ID").getAsString());
-        noeDTO.setNAME(json.get("NAME").getAsString());
-        return noeDTO;
-    }
-
-    public MezaDTO aldo() throws  IOException {
-        JsonParser aar = new JsonParser();
-        JsonObject json = (JsonObject) aar.parse(httpService.sendRequestHttpS("https://aldodto.free.beeceptor.com", "GET", null, null, "json", null, null));
-        MezaDTO aldoDTO = new MezaDTO();
-        aldoDTO.setStatus(json.get("status").getAsString());
-        aldoDTO.setId(json.get("id").getAsInt());
-        aldoDTO.setName(json.get("name").getAsString());
-        return aldoDTO;
-    }
-
-    public FragosoDTO david() throws  IOException {
-        JsonParser por = new JsonParser();
-        JsonObject json = (JsonObject) por.parse(httpService.sendRequestHttpS("https://davidendpoint.free.beeceptor.com", "GET", null, null, "json", null, null));
-        FragosoDTO davidDTO = new FragosoDTO();
-        davidDTO.setStatus(json.get("status").getAsString());
-        davidDTO.setId(json.get("id").getAsInt());
-        davidDTO.setName(json.get("name").getAsString());
-        return davidDTO;
-    }
-    public CejaDTO cesar() throws  IOException {
-        JsonParser ahhh = new JsonParser();
-        JsonObject json = (JsonObject) ahhh.parse(httpService.sendRequestHttpS("https://cesar.free.beeceptor.com", "GET", null, null, "json", null, null));
-        CejaDTO cesarDTO = new CejaDTO();
-        cesarDTO.setStatus(json.get("status").getAsString());
-        cesarDTO.setId(json.get("id").getAsInt());
-        cesarDTO.setName(json.get("name").getAsString());
-        return cesarDTO;
-    }
 }
---
+++ src/main/java/com/ut/sm42/service/impl/ApplicationServiceImpl.java
@@ -0,0 +1,76 @@
+package com.ut.sm42.service.impl;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ut.sm42.dto.*;
+import com.ut.sm42.exception.BusinessException;
+import com.ut.sm42.service.ApplicationService;
+import com.ut.sm42.service.HttpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+public class ApplicationServiceImpl implements ApplicationService {
+    @Autowired
+    HttpService httpService;
+
+
+    @Override
+    public String firstService(){
+        return "service";
+    }
+
+
+    @Override
+    public BeeceptorDTO testHttp() throws IOException {
+        JsonParser parser = new JsonParser();
+        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm42.free.beeceptor.com","GET",null,null,"json",null, null));
+        BeeceptorDTO beeceptorDTO = new BeeceptorDTO();
+
+        if(json.get("code")== null){
+            throw new BusinessException("Code doesn´t exist", HttpStatus.FORBIDDEN);
+        }
+        beeceptorDTO.setCode(json.get("code").getAsString());
+        beeceptorDTO.setMessage(json.get("message").getAsString());
+        beeceptorDTO.setStatus(json.get("status").getAsString());
+        return beeceptorDTO;
+    }
+    public NoeliDTO noe() throws  IOException{
+        JsonParser par = new JsonParser();
+        JsonObject json = (JsonObject) par.parse(httpService.sendRequestHttpS("https://equiponoe.free.beeceptor.com","GET",null,null,"json",null, null));
+        NoeliDTO noeDTO = new NoeliDTO();
+        noeDTO.setStatus(json.get("status").getAsString());
+        noeDTO.setID(json.get("ID").getAsString());
+        noeDTO.setNAME(json.get("NAME").getAsString());
+        return noeDTO;
+    }
+
+    public MezaDTO aldo() throws  IOException {
+        JsonParser aar = new JsonParser();
+        JsonObject json = (JsonObject) aar.parse(httpService.sendRequestHttpS("https://aldodto.free.beeceptor.com", "GET", null, null, "json", null, null));
+        MezaDTO aldoDTO = new MezaDTO();
+        aldoDTO.setStatus(json.get("status").getAsString());
+        aldoDTO.setId(json.get("id").getAsInt());
+        aldoDTO.setName(json.get("name").getAsString());
+        return aldoDTO;
+    }
+
+    public FragosoDTO david() throws  IOException {
+        JsonParser por = new JsonParser();
+        JsonObject json = (JsonObject) por.parse(httpService.sendRequestHttpS("https://davidendpoint.free.beeceptor.com", "GET", null, null, "json", null, null));
+        FragosoDTO davidDTO = new FragosoDTO();
+        davidDTO.setStatus(json.get("status").getAsString());
+        davidDTO.setId(json.get("id").getAsInt());
+        davidDTO.setName(json.get("name").getAsString());
+        return davidDTO;
+    }
+    public CejaDTO cesar() throws  IOException {
+        JsonParser ahhh = new JsonParser();
+        JsonObject json = (JsonObject) ahhh.parse(httpService.sendRequestHttpS("https://cesar.free.beeceptor.com", "GET", null, null, "json", null, null));
+        CejaDTO cesarDTO = new CejaDTO();
+        cesarDTO.setStatus(json.get("status").getAsString());
+        cesarDTO.setId(json.get("id").getAsInt());
+        cesarDTO.setName(json.get("name").getAsString());
+        return cesarDTO;
+    }
+}
