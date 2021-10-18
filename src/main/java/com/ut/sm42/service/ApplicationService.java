9893e250116202bb6769fb593052b068d1202f08
Diegohuchim17 <perezaledi@gmail.com>
2021-10-18 18:15:37 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/service/ApplicationService.java
+++ src/main/java/com/ut/sm42/service/ApplicationService.java
@@ -13,69 +13,19 @@ import com.ut.sm42.dto.*;
 import java.io.IOException;
 
 @Service
-public class ApplicationService {
+public interface ApplicationService {
 
-    @Autowired
-    HttpService httpService;
+    String firstService();
 
-    public String firstService() {
-        return "service";
-    }
+    BeeceptorDTO testHttp() throws IOException;
 
-    public BeeceptorDTO testHttp() throws IOException {
-        JsonParser parser = new JsonParser();
-        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm42.free.beeceptor.com", "GET", null, null, "json", null, null));
-        BeeceptorDTO beeceptorDTO = new BeeceptorDTO();
+    HuchimDTO diego() throws IOException;
 
-        if (json.get("code") == null) {
-            throw new BusinessException("Code doesn´t exist", HttpStatus.FORBIDDEN);
-        }
-        beeceptorDTO.setCode(json.get("code").getAsString());
-        beeceptorDTO.setMessage(json.get("message").getAsString());
-        beeceptorDTO.setStatus(json.get("status").getAsString());
-        return beeceptorDTO;
-    }
 
-    public HuchimDTO diego() throws IOException {
-        JsonParser asd = new JsonParser();
-        JsonObject json = (JsonObject) asd.parse(httpService.sendRequestHttpS("https://huchim.free.beeceptor.com", "GET", null, null, "json", null, null));
-        HuchimDTO diegoDTO = new HuchimDTO();
-        diegoDTO.setId(json.get("id").getAsInt());
-        diegoDTO.setName(json.get("name").getAsString());
-        diegoDTO.setStatus(json.get("Status").getAsString());
-        return diegoDTO;
+    AranaDTO ivan() throws IOException;
 
-    }
+    EscobarDTO cruz() throws IOException;
 
-    public AranaDTO ivan() throws IOException {
-        JsonParser asd = new JsonParser();
-        JsonObject json = (JsonObject) asd.parse(httpService.sendRequestHttpS("https://arana.free.beeceptor.com", "GET", null, null, "json", null, null));
-        AranaDTO ivanDTO = new AranaDTO();
-        ivanDTO.setIdentificador(json.get("identificador").getAsInt());
-        ivanDTO.setNombre(json.get("Nombre").getAsString());
-        ivanDTO.setStatus(json.get("status").getAsString());
-        return ivanDTO;
-    }
+    LairDTO carlos() throws IOException;
 
-    public EscobarDTO cruz() throws IOException {
-        JsonParser asd = new JsonParser();
-        JsonObject json = (JsonObject) asd.parse(httpService.sendRequestHttpS("https://eduardoescobar.free.beeceptor.com/", "GET", null, null, "json", null, null));
-        EscobarDTO cruzDTO = new EscobarDTO();
-        cruzDTO.setStatus(json.get("Status").getAsString());
-        cruzDTO.setId(json.get("id").getAsInt());
-        cruzDTO.setName(json.get("name").getAsString());
-        return cruzDTO;
-
-    }
-
-    public LairDTO carlos() throws IOException {
-        JsonParser asd = new JsonParser();
-        JsonObject json = (JsonObject) asd.parse(httpService.sendRequestHttpS("https://carlos.free.beeceptor.com", "GET", null, null, "json", null, null));
-        LairDTO carlosDTO = new LairDTO();
-        carlosDTO.setId(json.get("id").getAsInt());
-        carlosDTO.setName(json.get("name").getAsString());
-        carlosDTO.setStatus(json.get("Status").getAsString());
-        return carlosDTO;
-
-    }
 }
