934c490a23ea14014d3985e5dcaf95f198f1c862
sebem <20393097@utcancun.edu.mx>
2021-10-18 13:49:50 -0500

    Feat (ApplicationServiceImpl) add new security level

--- src/main/java/com/ut/sm42/service/ApplicationService.java
+++ src/main/java/com/ut/sm42/service/ApplicationService.java
@@ -12,63 +12,11 @@ import com.ut.sm42.dto.*;
 import java.io.IOException;
 
 @Service
-public class ApplicationService {
+public interface ApplicationService {
 
-    @Autowired
-    HttpService httpService;
 
-    public String firstService(){
-        return "service";
-    }
+    String firstService();
+    BeeceptorDTO testHttp() throws IOException;
 
-    public BeeceptorDTO testHttp() throws IOException {
-        JsonParser parser = new JsonParser();
-        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm42.free.beeceptor.com","GET",null,null,"json",null, null));
-        BeeceptorDTO beeceptorDTO = new BeeceptorDTO();
-
-        if(json.get("code")== null){
-            throw new BusinessException("Code doesn´t exist", HttpStatus.FORBIDDEN);
-        }
-        beeceptorDTO.setCode(json.get("code").getAsString());
-        beeceptorDTO.setMessage(json.get("message").getAsString());
-        beeceptorDTO.setStatus(json.get("status").getAsString());
-        return beeceptorDTO;
-    }
-    public RomerithoDTO sebastiane() throws IOException {
-        JsonParser parcero = new JsonParser();
-        JsonObject json = (JsonObject) parcero.parse(httpService.sendRequestHttpS("https://romar.free.beeceptor.com","GET",null,null,"json",null, null));
-        RomerithoDTO htqDTO = new RomerithoDTO();
-        htqDTO.setId(json.get("Id").getAsInt());
-        htqDTO.setName(json.get("Name").getAsString());
-        htqDTO.setStatus(json.get("status").getAsString());
-        return htqDTO;
-    }
-
-    public MartinDTO erick() throws IOException {
-        JsonParser crack = new JsonParser();
-        JsonObject json = (JsonObject) crack.parse(httpService.sendRequestHttpS("https://erickr.free.beeceptor.com","GET",null,null,"json",null, null));
-        MartinDTO ersDTO = new MartinDTO();
-        ersDTO.setId(json.get("Id").getAsInt());
-        ersDTO.setName(json.get("Name").getAsString());
-        ersDTO.setStatus(json.get("status").getAsString());
-        return ersDTO;
-    }
-    public TecDTO jesus() throws IOException {
-        JsonParser am= new JsonParser();
-        JsonObject json = (JsonObject) am.parse(httpService.sendRequestHttpS("https://yisus.free.beeceptor.com","GET",null,null,"json",null, null));
-        TecDTO yisusDTO = new TecDTO();
-        yisusDTO.setId(json.get("Id").getAsInt());
-        yisusDTO.setName(json.get("Name").getAsString());
-        yisusDTO.setStatus(json.get("status").getAsString());
-        return yisusDTO;
-    }
-    public RamirezDTO OscarL() throws IOException {
-        JsonParser parcera = new JsonParser();
-        JsonObject json = (JsonObject) parcera.parse(httpService.sendRequestHttpS("https://lemons.free.beeceptor.com","GET",null,null,"json",null, null));
-        RamirezDTO lemon = new RamirezDTO();
-        lemon.setId(json.get("Id").getAsInt());
-        lemon.setName(json.get("Name").getAsString());
-        lemon.setStatus(json.get("status").getAsString());
-        return lemon;
-    }
 }
+
