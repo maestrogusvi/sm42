58d59570d13efed69db466f2919ebbd454b07d39
gerryr8 <camaragerardo2000@gmail.com>
2021-10-19 22:36:57 -0500

    Feat(ApplicationServiceImpl) add new security level Impl

--- src/main/java/com/ut/sm42/service/ApplicationService.java
+++ src/main/java/com/ut/sm42/service/ApplicationService.java
@@ -10,36 +10,13 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
-public class ApplicationService {
-
-    @Autowired
-    HttpService httpService;
-
-    public String firstService(){
-        return "service";
-    }
-
-    public BeeceptorDTO testHttp() throws IOException {
-        JsonParser parser = new JsonParser();
-        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://utsm41.free.beeceptor.com","GET",null,null,"json",null, null));
-        BeeceptorDTO beeceptorDTO = new BeeceptorDTO();
-        beeceptorDTO.setCode(json.get("code").getAsString());
-        beeceptorDTO.setMessage(json.get("message").getAsString());
-        beeceptorDTO.setStatus(json.get("status").getAsString());
-        return beeceptorDTO;
-    }
+public interface ApplicationService  {
 
 
-    public CamaraDTO gerardo () throws IOException {
-        JsonParser asd = new JsonParser();
-        JsonObject json = (JsonObject) asd.parse(httpService.sendRequestHttpS("https://gerardo.free.beeceptor.com", "GET",null,null,"json",null, null));
-        CamaraDTO gerardoDTO = new CamaraDTO();
-        gerardoDTO.setId(json.get("id").getAsInt());
-        gerardoDTO.setName(json.get("name").getAsString());
-        gerardoDTO.setStatus(json.get("status").getAsString());
-        return gerardoDTO;
 
-    }
+    String firstService();
+     BeeceptorDTO testHttp() throws IOException;
+     CamaraDTO gerardo () throws IOException;
 
 
 }
