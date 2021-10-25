969e46826ecd805cb313bd59bd632d551d78da1f
xlami <Ourycz2002>
2021-10-24 22:21:41 -0500

    FIX (MartinDTO).

--- src/main/java/com/ut/sm42/dto/TecDTO.java
+++ src/main/java/com/ut/sm42/dto/TecDTO.java
@@ -3,40 +3,45 @@ public class TecDTO {
     String Status;
     int id;
     String name;
+
     public TecDTO() {
     }
+
     public String getStatus() {
         return Status;
     }
+
     public void setStatus(String status) {
         Status = status;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
 
-        @Override
-        public String toString() {
-            return "TecDTO{" +
-                    "status='" + name + '\'' +
-                    ", code='" + id + '\'' +
-                    ", message='" + name + '\'' +
-                    '}';
-        }
-
-        public String toJSON() {
-            return "{" +
-                    "\"status\"=" + "\"" + Status + "\"," +
-                    "\"code\"=" + "\"" + id + "\"," +
-                    "\"message\"=" + "\"" + name + "\"," +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "TecDTO{" +
+                "Status='" + Status + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
+
+    public String TecDTO() {
+        return "{" +
+                "\"status\"=" + "\"" + Status + "\"," +
+                "\"code\"=" + "\"" + id + "\"," +
+                "\"message\"=" + "\"" + name + "\"," +
+                '}';
+
+    }
+}
