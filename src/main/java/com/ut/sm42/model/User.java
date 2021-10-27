02d31d01224f51dcb34a62b066570ed0fd0bebc6
Diegohuchim17 <perezaledi@gmail.com>
2021-10-26 19:50:52 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/model/User.java
+++ src/main/java/com/ut/sm42/model/User.java
@@ -1,4 +1,43 @@
 package com.ut.sm42.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    int id;
+    String name;
+    String status;
+
+
+    public User(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
