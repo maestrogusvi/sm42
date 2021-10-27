d9af9adef49a702886468274c57d4ceb032f4b5d
sebem <20393097@utcancun.edu.mx>
2021-10-27 08:00:00 -0500

    FIX (pom.xml) FIX (User) Fix (UserRepository)

---
+++ src/main/java/com/ut/sm42/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ut.sm42.user;
+
+import com.ut.sm42.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
