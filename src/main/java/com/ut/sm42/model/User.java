833bf0334e95d6dca6624857e5934cb0c9361d7d
lorddeivis <fragosomadera99@gmail.com>
2021-10-27 08:32:29 -0500

    add(model/user)

---
+++ src/main/java/com/ut/sm42/model/User.java
@@ -0,0 +1,46 @@
+package com.ut.sm42.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    String status;
+
+
+    public User(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
