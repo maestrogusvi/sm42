ff88429500ccbdf3ac8253147e2af435bf049c3e
davida209 <84547400+davida209@users.noreply.github.com>
2021-10-26 23:18:34 -0500

    Update UserRepository.java

--- src/main/java/com/ut/sm42/repository/UserRepository.java
+++ src/main/java/com/ut/sm42/repository/UserRepository.java
@@ -1,13 +1,10 @@
 package com.ut.sm42.repository;
 
 import com.ut.sm42.model.User;
-//profe aqui no le supimos muy bien no entendimos vimos la clase 10 veces
-//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
 
+public interface UserRepository extends JpaRepository <User, Integer> {
 }
-
