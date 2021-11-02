697a6527820e045833d657f9b01ea08dba03a664
Diegohuchim17 <perezaledi@gmail.com>
2021-11-02 14:18:54 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/dto/MediaStackPaginationDTO.java
+++ src/main/java/com/ut/sm42/dto/MediaStackPaginationDTO.java
@@ -8,7 +8,38 @@ public class MediaStackPaginationDTO {
     int count;
     int total;
 
+    public MediaStackPaginationDTO() {
+    }
 
+    public int getLimit() {
+        return limit;
+    }
 
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
