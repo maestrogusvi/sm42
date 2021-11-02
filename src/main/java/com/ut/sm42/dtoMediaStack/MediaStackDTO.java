6f98fcad67c535b4da83d6702e5469cef4754a71
Diegohuchim17 <perezaledi@gmail.com>
2021-11-02 14:46:21 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/dtoMediaStack/MediaStackDTO.java
+++ src/main/java/com/ut/sm42/dtoMediaStack/MediaStackDTO.java
@@ -1,6 +1,29 @@
 package com.ut.sm42.dtoMediaStack;
 
+import java.util.List;
+
 public class MediaStackDTO {
 
+    MediaStackPaginationDTO mediaStackPaginationDTO;
+    List<MediaStackNewsDTO> mediaStackNewsDTOS;
 
+
+    public MediaStackDTO() {
+    }
+
+    public MediaStackPaginationDTO getMediaStackPaginationDTO() {
+        return mediaStackPaginationDTO;
+    }
+
+    public void setMediaStackPaginationDTO(MediaStackPaginationDTO mediaStackPaginationDTO) {
+        this.mediaStackPaginationDTO = mediaStackPaginationDTO;
+    }
+
+    public List<MediaStackNewsDTO> getMediaStackNewsDTOS() {
+        return mediaStackNewsDTOS;
+    }
+
+    public void setMediaStackNewsDTOS(List<MediaStackNewsDTO> mediaStackNewsDTOS) {
+        this.mediaStackNewsDTOS = mediaStackNewsDTOS;
+    }
 }
