dbd0a5f1ff5b9fb2883d7786d425d0c9ff507545
Diegohuchim17 <perezaledi@gmail.com>
2021-11-11 18:23:22 -0500

    FEAT(HuchimDTO)

---
+++ src/main/java/com/ut/sm42/dto/dtoTwitch/GameDTO.java
@@ -0,0 +1,45 @@
+package com.ut.sm42.dto.dtoTwitch;
+
+public class GameDTO {
+
+    String client_id;
+    String client_secret;
+    String grant_type;
+    String type;
+
+    public GameDTO() {
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getGrant_type() {
+        return grant_type;
+    }
+
+    public void setGrant_type(String grant_type) {
+        this.grant_type = grant_type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
