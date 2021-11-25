83a2d6f19f1461f60639f6740e2fdad2af274432
Diegohuchim17 <perezaledi@gmail.com>
2021-11-24 20:18:17 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/model/User.java
+++ src/main/java/com/ut/sm42/model/User.java
@@ -1,4 +1,61 @@
 package com.ut.sm42.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String status;
+    private String name;
+    private Short role;
+    private String password;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getRole() {
+        return role;
+    }
+
+    public void setRole(Short role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
