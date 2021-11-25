9786a7d2b00c3c811372de3971edb7a4b0f48fa5
Diegohuchim17 <perezaledi@gmail.com>
2021-11-24 22:03:25 -0500

    FEAT(HuchimDTO)

---
+++ src/main/java/com/ut/sm42/resource/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.ut.sm42.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import voltux.domain.reservation.User;
+import voltux.domain.reservation.UserDTO;
+import voltux.repository.UserRepository;
+import voltux.security.exception.BusinessException;
+import voltux.security.response.GenericResponse;
+import voltux.security.service.AuthenticationService;
+
+import static voltux.security.constants.AuthenticationConstants.URL_PRIVATE_AUTHETICATION;
+
+@RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS })
+@RequestMapping(path = URL_PRIVATE_AUTHETICATION, produces= MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<GenericResponse>  login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+        return new ResponseEntity<>(new GenericResponse(200,"success",auhtenticationService.loginAuthentication(username,pwd)), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/api/v1/user")
+    public @ResponseBody UserDTO newUser(@RequestBody User user) {
+        return auhtenticationService.createUser(user);
+    }
+
+
+    @GetMapping({ URL_PRIVATE_AUTHETICATION, "/voltux" })
+    public String index(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("Hello, %s!", jwt.getSubject());
+    }
+
+    @GetMapping("/api/v1/especialrequest/test")
+    String test(){
+        return "Success";
+    }
+
+
+
+}
+
