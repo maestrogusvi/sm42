1054f0274bbcfdbd170c8420598b06426b228a5c
gerryr8 <camaragerardo2000@gmail.com>
2021-11-26 06:53:26 -0500

    Feat(ApplicationServiceImpl) add new security level Impl

--- src/main/java/com/ut/sm42/service/AuthenticationService.java
+++ src/main/java/com/ut/sm42/service/AuthenticationService.java
@@ -1,4 +1,10 @@
 package com.ut.sm42.service;
 
+import com.ut.sm42.dto.UserDTO;
+import com.ut.sm42.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
 public interface AuthenticationService {
+    @Transactional
+    UserDTO createUser(User entity);
 }
