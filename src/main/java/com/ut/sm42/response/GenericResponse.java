e825ed3be8c95b8240fa6b88832d545c9d356110
Diegohuchim17 <perezaledi@gmail.com>
2021-11-26 23:30:10 -0500

    FEAT(HuchimDTO)

--- src/main/java/com/ut/sm42/response/GenericResponse.java
+++ src/main/java/com/ut/sm42/response/GenericResponse.java
@@ -1,4 +1,38 @@
 package com.ut.sm42.response;
 
 public class GenericResponse {
+
+    int code;
+    String message;
+    Object gere;
+
+    public GenericResponse(int code, String message, Object gere) {
+        this.code = code;
+        this.message = message;
+        this.gere = gere;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getGere() {
+        return gere;
+    }
+
+    public void setGere(Object gere) {
+        this.gere = gere;
+    }
 }
