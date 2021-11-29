82f4a4dd283893a3d6045d2a907ad20996eed5eb
diegohcuhim17 <perezaledi@gmail.com>
2021-11-28 22:10:53 -0500

    Huchim

--- src/main/java/com/ut/sm42/controller/ApplicationController.java
+++ src/main/java/com/ut/sm42/controller/ApplicationController.java
@@ -12,7 +12,6 @@ import com.ut.sm42.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 
 @RestController
